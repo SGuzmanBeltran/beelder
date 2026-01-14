8b455148b09caed4e4315c031bb85ed259d7b385
SGuzmanBeltran <santiagoguzmanbeltran@gmail.com>
2026-01-14 17:22:10 -0500

    feat: add VITE_STAGE argument for staging environment in Dockerfile and update main.tsx for dynamic title

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,11 +1,17 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
+import "./index.css";
 
-import "./index.css"
-import App from "./App.tsx"
+import App from "./App.tsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+// Set title based on environment
+const stage = import.meta.env.VITE_STAGE;
+if (stage && stage !== "production") {
+	document.title = `Beelder - ${stage}`;
+}
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
